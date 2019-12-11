d5f03fc05b974660f9ae03b63b3d66e68f91e879
ido595 <ido@superup.me>
2019-12-11 22:13:06 +0200

    start to implement linear regression

--- test.py
+++ test.py
@@ -83,3 +83,89 @@ continuous_vars = ['avg_final_grades', 'num_of_testers', ]
 df = pd.DataFrame(school_data_set)  # dataframe for easier handling of the data.
 refactor_data_frame(df)
 print(df)
+
+# making the dataframe easier.
+def linear_reg(data_frame):
+    columns_pos = {'avg_final_grades': 0,
+                   'num_of_testers': 1,
+                   'units': 2,
+                   'grad_year': 3,
+                   'profession': 4,
+                   'City city_name': 5,
+                   'school_name': 6}
+
+    # changing column names for easier work.
+    data_frame.rename(columns={'Final Grade Average': 'avg_final_grades',
+                               'Number of Testees': 'num_of_testers',
+                               'Yehidut Limud': 'units',
+                               'Graduation': 'grad_year',
+                               'Profession': 'profession',
+                               'City Name': 'city_name',
+                               'School Name': 'school_name',
+                               'School ID': 'school_id'},
+                      inplace=True)
+
+    # adding numeric columns to dataframe instead of nasty hebrew ones.
+    unique_prof = data_frame['profession'].unique()
+    unique_cities = data_frame['city_name'].unique()
+    unique_schools = data_frame['school_name'].unique()
+
+    features = 3 + unique_prof.size + unique_cities.size + unique_schools.size
+
+    unique_prof_dict = dict(
+        (val, index + 3) for index, val in enumerate(unique_prof))
+    unique_cities_dict = dict(
+        (val, index + 3 + unique_prof.size) for index, val in enumerate(unique_cities))
+    unique_schools_dict = dict(
+        (val, index + 3 + unique_prof.size + unique_cities.size) for index, val in enumerate(unique_schools))
+
+    data_x = np.ndarray(
+        shape=(data_frame.size, features),
+        dtype=float,
+        order='F')
+
+    data_y = np.ndarray(
+        shape=data_frame.size,
+        dtype=float,
+        order='F')
+
+    for index, row in df.iterrows():
+        data_y[index] = row['avg_final_grades']
+
+        data_x[index][0] = row['num_of_testers']
+        data_x[index][1] = row['units']
+        data_x[index][2] = row['grad_year']
+
+        data_x[index][unique_prof_dict[row['profession']]] = 1
+        data_x[index][unique_cities_dict[row['city_name']]] = 1
+        data_x[index][unique_schools_dict[row['school_name']]] = 1
+        print(index)
+
+    #np.random.shuffle(data_x)
+
+    x = tf.placeholder(tf.float32, [None, features])
+    y_ = tf.placeholder(tf.float32, [None, 1])
+    w = tf.Variable(tf.zeros([features, 1]))
+    b = tf.Variable(tf.zeros([1]))
+    y = tf.matmul(x, w) + b
+
+    loss = tf.reduce_mean(tf.pow(y - y_, 2)) + 0.1 * tf.nn.l2_loss(w)
+    update = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    for i in range(0, 1000):
+        sess.run(update, feed_dict={x: data_x, y_: data_y})
+        if i % 100 == 0:
+            print('Iteration:', i, ' W:', sess.run(w), ' b:', sess.run(b), ' loss:',
+                  loss.eval(session=sess, feed_dict={x: data_x, y_: data_y}))
+    #x_axis = np.arange(0, 8, 0.1)
+    #x_data = []
+    #for i in x_axis:
+    #    x_data.append(vecto(i))
+    #x_data = np.array(x_data)
+    #y_vals = np.matmul(x_data, sess.run(w)) + sess.run(b)
+    #import matplotlib.pyplot as plt
+    #plt.plot(x_axis, y_vals)
+    #plt.show()
+
